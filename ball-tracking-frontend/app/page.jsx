ea8a68c4db1eb642abde0ccd1f628347d4a5f1bf
SportsFacts <bm203174@gmail.com>
2026-02-14 17:05:17 +0530

    Integrate Tailwind CSS into Page component
    Added a Tailwind CSS styled div to the Page component.

--- ball-tracking-frontend/app/page.jsx
+++ ball-tracking-frontend/app/page.jsx
@@ -1,6 +1,10 @@
 import BallTrackingApp from "../components/BallTrackingApp";
 
 export default function Page() {
-  return <BallTrackingApp />;
+  return <BallTrackingApp />; 
+  <div className="bg-red-500 text-white text-5xl p-10">
+      Tailwind Working
+    </div>
 }
+
 
