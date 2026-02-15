dd516aff72a171a28ed9faf81cd4d7358a359cb8
SportsFacts <bm203174@gmail.com>
2026-02-15 17:40:08 +0530

    Update page.jsx

--- ball-tracking-frontend/app/page.jsx
+++ ball-tracking-frontend/app/page.jsx
@@ -1,10 +1,7 @@
 import BallTrackingApp from "../components/BallTrackingApp";
 
 export default function Page() {
-  return <BallTrackingApp />; 
-  <div className="bg-red-500 text-white text-5xl p-10">
-      Tailwind Working
-    </div>
+  return <BallTrackingApp />;
 }
 
 
